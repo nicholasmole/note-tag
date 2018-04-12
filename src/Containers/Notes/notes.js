31823d9bd12b47b657772ec96e2f12529add142a
Nick Mole <nmole065@g.rwu.edu>
2018-04-12 09:02:36 -0400

    initial setup of divs

---
+++ src/Containers/Notes/notes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Notes_TopBar_Options.css';
+
+export default () => (
+  <div>
+     <div className="Notes_TopBar_Options">Notes Topbar Option</div>
+     <div className="Notes_Contain_Notes_And_Explorer">
+      <div className="Notes_Explorer_Side_Part">Notes Explorer Side Part</div>
+      <div className="Notes_Core_Notes">Core Notes</div> 
+     </div>
+  </div>
+
+);
