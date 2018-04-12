de413ccb9aa898304062ce0fbcdb986386c5cd46
Nick Mole <nmole065@g.rwu.edu>
2018-04-12 13:34:11 -0400

    add italics button

--- src/Containers/Notes/CoreNotes/coreNotes.js
+++ src/Containers/Notes/CoreNotes/coreNotes.js
@@ -1,24 +1,34 @@
 import React, {Component} from 'react';
 
+let oDoc, sDefTxt;
 
 class CoreNotes extends Component {
+
+	constructor(props) {
+		super(props);
+		oDoc = document.getElementById("textBox");
+		sDefTxt = oDoc;
+	}
+	validateMode() {
+		// if (!document.compForm.switchMode.checked) { return true ; }
+		return true;
+		alert("Uncheck \"Show HTML\".");
+		oDoc.focus();
+		return false;
+	}
+	formatDoc = (sCmd, sValue) => {
+		if (this.validateMode()) { document.execCommand(sCmd, false, sValue); 
+			// oDoc.focus(); 
+		}
+	}
+
 	render() {
     return (
-<div id="wysiwyg" >
-  <input type="button" value="B" onclick="document.execCommand('bold', false, '');"/>
-   
-  <input type="button" value="I" onclick="document.execCommand('italic', false, '');"/>
-   
-  <input type="button" value="U" onclick="document.execCommand('underline', false, '');" />
-   
-  <input type="button" value="delete" onclick="document.execCommand('delete', false, '');" />
-   
-  <input type="button" value="link" onclick="document.execCommand('createLink', false, 'http://google.com');" />
-   
-  <input type="button" value="image" onclick="document.execCommand('insertImage', false, 'http://lorempixel.com/40/20/sports/')" />
-   
-  <div contenteditable="true">Lorem Ipsum is simply dummy text of the printing and typesetting industry</div>
-</div>
+			<div id="wysiwyg" >
+				<option value="h1">Title 1 &lt;h1&gt;</option>
+				<a href="#" data-command='commandName' onClick={()=>this.formatDoc('italic')}><i>i</i></a>
+				<div id="textBox" contentEditable="true">Lorem Ipsum is simply dummy text of the printing and typesetting industry</div>
+			</div>
 		)
 	}
 }
